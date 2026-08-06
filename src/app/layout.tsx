355a06148e5944ce8406fa92d7c07f57e27f7ee0
Code With Kashyap <codewithadityakashyap@gmail.com>
2026-08-06 08:49:47 +0530

    Chore: Integrate Google Analytics 4 tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -106,6 +106,22 @@ export default function RootLayout({
             })
           }}
         />
+        
+        {/* Google Analytics 4 */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
       </head>
       <body className={`${inter.variable} font-sans antialiased min-h-screen bg-background text-foreground`}>
         <ThemeProvider
